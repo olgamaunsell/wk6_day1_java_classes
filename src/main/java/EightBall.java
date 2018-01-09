d70ccd9b2947c5631c38145e023672b3a008b1d7
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-01-09 08:44:43 +0000

    Initial commit

---
+++ src/main/java/EightBall.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class EightBall {
+
+
+    private ArrayList<String> answers;
+
+    public EightBall(){
+        this.answers = new ArrayList<>();
+
+    }
+
+    public void addAnswer(String answer) {
+        this.answers.add(answer);
+    }
+
+    public void removeAnswer(String answer){
+        this.answers.remove()
+    }
+}
---
+++ src/test/java/EightBallTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+
+public class EightBallTest {
+
+    EightBall eightBall;
+
+    @Before
+    public void before(){
+        eightball = new EightBall();
+
+    }
+}
+
